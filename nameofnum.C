10218f1777b7ba8e80182619f7c56b65db3247d0
sandhiyakarthicsaran <35221190+sandhiyakarthicsaran@users.noreply.github.com>
2018-03-02 03:15:36 -0800

    name of the number

---
+++ nameofnum.C
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter the number:\n");
+scanf("%d",&n);
+switc(n)
+{
+case 1:
+printf("ONE\n");
+break;
+case 2:
+printf("TWO\n");
+break;
+case 3:
+printf("THREE\n");
+break;
+case 4:
+printf("FOUR\n");
+break;
+case 5:
+printf("FIVE\n");
+break;
+case 6:
+printf("SIX\n");
+break;
+case 7:
+printf("SEVEN\n");
+break;
+case8:
+printf("EIGHT\n");
+break;
+case9:
+printf("NINE\n");
+break;
+default:
+printf("The number is negative or invalid!!!\n");
+}
+getch();
+}
